778a0b76c515674781d7421b9fd132243e76c1fd
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-07 10:32:00 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,6 +6,7 @@ dnf config-manager --set-enabled crb
 dnf install epel-release -y
 dnf config-manager --set-enabled epel
 dnf group install "KDE Plasma Workspaces" -y
+dnf install sddm -y
 systemctl enable sddm
 systemctl set-default graphical.target
 dnf install flatpak git distrobox fuse -y
