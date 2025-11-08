0d490c013c12aa715b66e0b459338f24eb92d740
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-08 11:00:30 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-dnf install distrobox fastfetch git restic -y
+dnf install distrobox fastfetch git restic buildah -y
 dnf install gwenview kweather kate kcalc kontact okular skanpage -y # haruna
 dnf install fish -y
 
@@ -10,7 +10,19 @@ dnf install fish -y
 
 dnf copr enable ublue-os/packages -y
 dnf install ublue-brew ublue-os-udev-rules -y
+dnf install ublue-polkit-rules ublue-os-just ublue-os-update-services ublue-setup-services uupd -y
 dnf copr disable ublue-os/packages -y
+
+systemctl disable mcelog.service
+systemctl --global enable podman-auto-update.timer
+systemctl disable rpm-ostree.service
+systemctl enable ublue-system-setup.service
+systemctl enable uupd.timer
+systemctl --global enable ublue-user-setup.service
+systemctl mask bootc-fetch-apply-updates.timer bootc-fetch-apply-updates.service
+authselect enable-feature with-fingerprint
+systemctl enable systemd-resolved.service
+
 
 
 dnf clean all
