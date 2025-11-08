9c0667d4ed0ea2dca35ebd084cfff666767b0517
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-08 16:55:02 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,10 +21,6 @@ dnf install ublue-brew ublue-os-udev-rules -y
 dnf install ublue-polkit-rules ublue-os-just ublue-os-update-services ublue-setup-services uupd ublue-bling ublue-rebase-helper -y
 dnf copr disable ublue-os/packages -y
 
-dnf copr enable sabbyblue/amber-branding -y
-dnf install fedora-logos --allowerasing -y
-dnf copr disable sabbyblue/amber-branding -y
-
 
 systemctl enable brew-setup.service
 systemctl disable mcelog.service
