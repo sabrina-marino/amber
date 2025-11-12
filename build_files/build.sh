67f2b1a68047af8deef3e732c75dd8c68f904f12
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-12 13:32:42 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,11 @@ set -ouex pipefail
 
 
 # rsync -rvK /ctx/system_files/ /
-
+dnf5 install wine -y
+dnf5 copr enable ycollet/audinux -y
+dnf5 install LinVst -y
+dnf5 copr disable ycollet/audinux -y
+dnf5 install realtime-setup -y
 dnf5 group install audio -y
 #dnf5 group install "Design Suite" -y
 dnf5 install fish zsh -y
@@ -13,5 +17,5 @@ dnf5 install rclone restic buildah fzf ntfs-3g -y
 dnf5 copr enable ublue-os/packages -y
 dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
 dnf5 copr disable ublue-os/packages -y
-dnf5 remove firefox firefox-langpacks PackageKit PackageKit-command-not-found -y
+dnf5 remove firefox firefox-langpacks -y
 dnf5 clean all
