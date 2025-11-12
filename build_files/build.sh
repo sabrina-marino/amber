eab4db9fc1d5c315489b9ba77262a688487bbd4d
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-12 13:10:53 -0300

    Fix typo

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ dnf5 install fish zsh -y
 dnf5 install ptyxis kweather kcalc kontact okular skanpage -y
 dnf5 install rclone restic buildah fzf ntfs-3g -y
 dnf5 copr enable ublue-os/packages -y
-dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-os-setup-services ublue-update-services uupd -y
+dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
 dnf5 copr disable ublue-os/packages -y
 dnf5 remove firefox firefox-langpacks PackageKit PackageKit-command-not-found -y
 dnf5 clean all
