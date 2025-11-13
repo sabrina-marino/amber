124e7e159ecf1e369a5598a1ff6b75dab59b680c
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-13 08:29:39 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ dnf5 group install audio -y
 #dnf5 group install "Design Suite" -y
 dnf5 install fish zsh -y
 dnf5 install ptyxis kweather kcalc kontact okular skanpage -y
-dnf5 install rclone restic buildah fzf ntfs-3g -y
+dnf5 install fastfetch rclone restic buildah fzf ntfs-3g -y
 dnf5 copr enable ublue-os/packages -y
 dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
 dnf5 copr disable ublue-os/packages -y
