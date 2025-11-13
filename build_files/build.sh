9afcfae112166d07447813ff5e381d174cc1f7d7
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-13 09:00:05 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,15 +4,15 @@ set -ouex pipefail
 
 
 # rsync -rvK /ctx/system_files/ /
-# dnf5 install wine -y
-dnf5 copr enable ycollet/audinux -y
-dnf5 install LinVst -y
-dnf5 copr disable ycollet/audinux -y
+
+# dnf5 copr enable ycollet/audinux -y
+# dnf5 install LinVst -y
+# dnf5 copr disable ycollet/audinux -y
 dnf5 install realtime-setup -y
 dnf5 group install audio -y
 #dnf5 group install "Design Suite" -y
 dnf5 install fish zsh -y
-dnf5 install ptyxis kweather kcalc kontact okular skanpage -y
+dnf5 install gwenview ptyxis kweather kcalc okular skanpage vlc -y
 dnf5 install fastfetch rclone restic buildah fzf ntfs-3g -y
 dnf5 copr enable ublue-os/packages -y
 dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
