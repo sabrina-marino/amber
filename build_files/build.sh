43f44a5aaa22904e36291cec50d200348fc4692a
Sabrina Marino <152991379+sabrina-marino@users.noreply.github.com>
2025-11-28 15:15:39 -0300

    fix dnf command

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,22 +5,22 @@ set -ouex pipefail
 
 # rsync -rvK /ctx/system_files/ /
 
-# dnf5 copr enable ycollet/audinux -y
-# dnf5 install LinVst -y
-# dnf5 copr disable ycollet/audinux -y
-# dnf5 install realtime-setup -y
-# dnf5 install python3-legacy-cgi -y # raysession dependency
-# dnf5 group install audio -y
-# dnf5 remove rakarrack -y # this one doesn't work
-#dnf5 group install "Design Suite" -y
-dnf5 install fish zsh -y
-dnf5 copr enable atim/starship -y
-dnf5 install starship -y
-dnf5 copr disable atim/starship -y
-dnf5 install gwenview ptyxis kweather kcalc okular skanpage vlc -y
-dnf5 install fastfetch rclone restic -y
-dnf5 copr enable ublue-os/packages -y
-dnf5 install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
-dnf5 copr disable ublue-os/packages -y
-dnf5 remove firefox firefox-langpacks -y
-dnf5 clean all
+# dnf copr enable ycollet/audinux -y
+# dnf install LinVst -y
+# dnf copr disable ycollet/audinux -y
+# dnf install realtime-setup -y
+# dnf install python3-legacy-cgi -y # raysession dependency
+# dnf group install audio -y
+# dnf remove rakarrack -y # this one doesn't work
+# dnf group install "Design Suite" -y
+dnf install fish zsh -y
+dnf copr enable atim/starship -y
+dnf install starship -y
+dnf copr disable atim/starship -y
+dnf install gwenview ptyxis kweather kcalc okular skanpage vlc -y
+dnf install fastfetch rclone restic -y
+dnf copr enable ublue-os/packages -y
+dnf install ublue-bling ublue-brew ublue-os-just ublue-rebase-helper ublue-setup-services ublue-os-update-services uupd -y
+dnf copr disable ublue-os/packages -y
+dnf remove firefox firefox-langpacks -y
+dnf clean all
